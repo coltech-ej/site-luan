302b73611c6816eb4a401293e8d54e02ef76ec86
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-06-18 17:09:28 -0400

    chore: add link to page portfolio-item

--- src/components/ShowProject/index.tsx
+++ src/components/ShowProject/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Flex, Grid, GridItem, Heading, Icon, Image } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem, Heading, Icon, Image, Text, Link } from "@chakra-ui/react";
+import { Link as RouterLink} from "react-router-dom";
 
 import { Logotipo } from "../Icons";
 
@@ -19,7 +20,7 @@ export function ShowProject({img, title}: IShowProjectProps) {
       py="4"
     >
       <Grid
-        templateRows="1fr 1fr"
+        templateRows="1fr 1fr 1fr"
         w="100%"
         h="100%"
       >
@@ -34,15 +35,38 @@ export function ShowProject({img, title}: IShowProjectProps) {
         </GridItem>
 
         <GridItem>
-          <Box 
-            w="36"
+          <Flex
+            direction="column"
+            textAlign="start"
           >
-            <Heading
-              textShadow='0.1em 0.1em 0.2em black' 
-              textAlign="start"
-            >{title}</Heading>
-          </Box>
+            <Box 
+              w="36"
+            >
+              <Heading
+                textShadow='0.1em 0.1em 0.2em black' 
+                textAlign="start"
+              >{title}</Heading>
+            </Box>
+            <Text
+              fontSize="lg"
+              textTransform="uppercase"
+              mt="4"
+            >Projeto Residencial</Text>
+
+            <Link as={RouterLink} to="/portifolio-item"
+              color="orange"
+              mt="4"
+              cursor="pointer"
+              _hover={{
+                filter: "brightness(150%)"
+              }}
+            >
+              Visualizar projeto
+            </Link>
+          </Flex>
         </GridItem>
+
+        <GridItem />
       </Grid>
     </Flex>
   );
