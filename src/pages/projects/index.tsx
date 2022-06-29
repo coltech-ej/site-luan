42c99e07c5f5cca1b28df97ca26d0c83c0f004c3
grvela <rodrigo@vela.localdomain>
2022-06-29 00:24:40 -0400

    fix(apollo):fixing re-render components

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,41 +1,39 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable unused-imports/no-unused-vars */
 import { GET_PROJECTS } from "../../api/querys";
 import { useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
+import { Header } from "../../components/Header";
+import { Flex, Center, Grid, GridItem, Text } from "@chakra-ui/react";
+import { Footer } from "../../components/Footer";
+import { Project } from "../../components/Project";
 
 export function Projects() {
-	const [projects, setProjects] = useState([
-		{
-			id: "",
-			name: "",
-			location: "",
-			url: "",
-		},
-	]);
+	const [projects, setProjects] = useState([]);
 	const { loading, error, data } = useQuery(GET_PROJECTS);
 
 	useEffect(() => {
 		if (data) {
 			const projectsArray = data.projects.data;
-			console.log(projectsArray);
-			projectsArray.map((project: any) => {
-				const constructer = {
-					id: project.id,
-					name: project.attributes.name,
-					location: project.attributes.basics.location,
-					url: project.attributes.banner.data.attributes.formats.large.url,
-				};
-				console.log(constructer);
-				setProjects([...projects, constructer]);
-			});
-			console.log(projects);
+			const constructor = projectsArray.map((project: any) => ({
+				id: project.id,
+				name: project.attributes.name,
+				location: project.attributes.basics.location,
+				url: "https://images.unsplash.com/photo-1522071500372-f0fd8c452178?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+				// url: project.attributes.banner.data.attributes.formats.large.url,
+			}));
+			setProjects(constructor);
 		}
-	}, [data]);
+	}, [loading]);
+
+	useEffect(() => {
+		console.log(projects);
+	}, [projects]);
 
 	if (loading) return <p>Loading...</p>;
 	if (error) return <p>Error : {error.message}</p>;
-	/*
+
 	return (
 		<>
 			<Header bgColor="gray.700" position="static" logo={true} />
@@ -57,19 +55,16 @@ export function Projects() {
 					</Center>
 					<Grid templateColumns="repeat(3, 1fr)" marginTop="20" gap="5">
 						{
-							/*data.map((project) => (
-							// eslint-disable-next-line react/jsx-key
-							<GridItem bgImage={project.url} bgSize="cover" w="100%" h="22em">
-								<Project name={project.name} location={project.location} />
-							</GridItem>
-						))
 							// eslint-disable-next-line @typescript-eslint/no-explicit-any
 							projects.map((project: any) => (
-								<GridItem bgImage={project.url} bgSize="cover" w="100%" h="22em">
-									<Project
-										name={project.name}
-										location={project.location}
-									/>
+								<GridItem
+									key={project.id}
+									bgImage={project.url}
+									bgSize="cover"
+									w="100%"
+									h="22em"
+								>
+									<Project name={project.name} location={project.location} />
 								</GridItem>
 							))
 						}
@@ -78,5 +73,5 @@ export function Projects() {
 			</Flex>
 			<Footer />
 		</>
-	);*/
+	);
 }
